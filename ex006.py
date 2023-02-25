be2e04b1483181186e12c02ee076729e3753535e
Sulamita Pereira <105397760+SulamitaPereira@users.noreply.github.com>
2023-02-25 13:04:59 -0300

    Create ex006.py
    Soma, multiplicação, divisão, sobra e divisão inteira.

---
+++ ex006.py
@@ -0,0 +1,9 @@
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+print('A soma é {}, o produto é {} \n e a divisão é {}' .format(s, m, d), end=' ')
+print('A divisão inteira equivale a {} \n e a exponenciação é igual a {}' .format(di, e))
