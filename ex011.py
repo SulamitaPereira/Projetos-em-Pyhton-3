11a768a78445ab3ae13b47b4bc714ff22a7c382a
Sulamita Pereira <105397760+SulamitaPereira@users.noreply.github.com>
2023-02-25 13:09:35 -0300

    Create ex011.py
    Tabuada.

---
+++ ex011.py
@@ -0,0 +1,14 @@
+n1 = int(input('Digite um número para descobrir qual é a sua tabuada: '))
+t0 = n1 * 1
+t1 = n1 * 2
+t2 = n1 * 3
+t3 = n1 * 4
+t4 = n1 * 5
+t5 = n1 * 6
+t6 = n1 * 7
+t7 = n1 * 8
+t8 = n1 * 9
+t9 = n1 * 10
+t09 = n1 * 11
+t10 = n1 * 12
+print('{} vezes 1, é igual {}. \n{} vezes 2, é igual a {}. \n{} vezes 3, é igual a {}. \n{} vezes 4, é igual a {}. \n{} vezes 5, é igual a {}. \n{} vezes 6, é igual a {}. \n{} vezes 7, é igual a {}. \n{} vezes 8, é igual a {}. \n{} vezes 9, é igual a {}. \n{} vezes 10, é igual a {}. \n{} vezes 11, é igual a {}. \n{} vezes 12, é igual a {}.' .format(n1, t0, n1, t1, n1, t2, n1, t3, n1, t4, n1, t5, n1, t6, n1, t7, n1, t8, n1, t9, n1, t09,n1, t10))
