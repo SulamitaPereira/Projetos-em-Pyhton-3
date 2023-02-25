928e25d26d26f03fcb7d1eb6f0abd3c92a8c6ab4
Sulamita Pereira <105397760+SulamitaPereira@users.noreply.github.com>
2023-02-25 13:50:30 -0300

    Create ex013.py
    Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário com 15% de aumento

---
+++ ex013.py
@@ -0,0 +1,4 @@
+s1 = float(input('Digite aqui o valor do seu salário: '))
+sa1 = s1 + (s1 / 15)
+print('No próximo mês, o seu salário sofrerá um reajuste de 15% do valor atual!')
+print('O novo valor do seu salário a partir do próximo mês será de: {:.2f}!' .format(sa1))
