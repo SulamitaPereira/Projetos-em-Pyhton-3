552b5f8553810b3a7b9dd4c3517bbe2053dd2919
Sulamita Pereira <105397760+SulamitaPereira@users.noreply.github.com>
2023-02-25 14:17:51 -0300

    Create ex016.py
    Dobro, triplo, raiz quadrada.

---
+++ ex016.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite um número: '))
+n2 = n1 * 2
+n3 = n1 * 3
+n4 = n1 ** (1/2)
+print('O dobro de {} é igual a {}. \nO triplo de {} é igual a {} \nE a raiz quadrada de {} é igual a {}.' .format(n1, n2, n1, n3, n1, n4))
