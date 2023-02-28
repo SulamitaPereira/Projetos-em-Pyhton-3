1e92ecbdcbb085617149cf8708a0635b0585e90c
Sulamita Pereira <105397760+SulamitaPereira@users.noreply.github.com>
2023-02-28 19:32:39 -0300

    Update ex020.py
    Calculando a hipotenusa (de duas formas)

--- ex020.py
+++ ex020.py
@@ -1,6 +1,12 @@
-import math
 oposto = float(input('Comprimento do cateto oposto: '))
 adjacente = float(input('Comprimento do cateto adjacente: '))
 hipotenusa = (oposto ** 2 + adjacente ** 2) ** (1/2)
+print('O valor da hipotenusa: {:.2f}' . format(hipotenusa))
 
+///
+
+import math
+oposto = float(input('Comprimento do cateto oposto: '))
+adjacente = float(input('Comprimenro do cateto adjacente: '))
+hipotenusa = math.hypot(oposto, adjacente)
 print('O valor da hipotenusa: {:.2f}' . format(hipotenusa))
