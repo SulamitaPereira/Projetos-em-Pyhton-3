ff984a369fece183fd272f2e5876306853820ef6
Sulamita Pereira <105397760+SulamitaPereira@users.noreply.github.com>
2023-02-28 20:19:22 -0300

    Create ex021.py
    Calculando seno, cosseno e tangente de um ângulo.

---
+++ ex021.py
@@ -0,0 +1,17 @@
+import math
+ang = float(input('Qual é o ângulo? '))
+seno = math.sin(math.radians(ang))
+cosseno = math.cos(math.radians (ang))
+tangente = math.tan(math.radians (ang))
+print ('O ÂNGULO tem o seno de {:.2f}\nO COSSENO de {:.2f} \nE a TANGENTE de {:.2f}' .format(seno, cosseno, tangente))
+
+///
+
+import math
+ang = float(input('Digite o valor do ângulo: ')
+seno = math.sin(math.radians(ang))
+cosseno = math.cos(math.radians(ang))
+tangente = math.tan(math.radians(ang))
+print ('O ângulo tem o SENO de {:.2f}' .format(seno))
+print ('O COSSENO de {:.2f}' .format(cosseno))
+print ('E a TANGENTE de {:.2f}' .format(tangente))
