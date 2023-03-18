dc4cf016f1a8923ef01169c7fd048189d3cb0489
Sulamita Pereira <105397760+SulamitaPereira@users.noreply.github.com>
2023-03-18 13:06:34 -0300

    Create Aula9.py
    Manipulando uma string

---
+++ Aula9.py
@@ -0,0 +1,15 @@
+### para exibir a 4ª letra: (s) ###
+frase = 'Curso em Video Python'
+print(frase [3])
+
+### para exibir da 4ª até a 12ª letra: (so em Vide)###
+frase = 'Curso em Video Python'
+print(frase [3:13])
+
+### para exibir do 0 até a 12ª: (Curso em Vide)###
+frase = 'Curso em Video Python'
+print(frase [:13])
+
+### da segunda letra a 15ª, com intervalo de 2 em 2###
+frase = 'Curso em Video Python'
+print(frase [1:15:2])
